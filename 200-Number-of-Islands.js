43e0041f62fc3ddfb2a866e1eeb266c2ba0fe14f
Mazen Mohamed <mazn34903@gmail.com>
2024-09-19 21:35:51 +0300

    [200] [Time Beats: 97.90%] [Memory Beats: 96.79%] - LeetPush

---
+++ 200-Number-of-Islands.js
@@ -0,0 +1,31 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    let islands = 0
+    for(let i = 0; i < grid.length ;i++){
+        for(let j = 0; j < grid[0].length ; j++){
+            if(grid[i][j] == \1\) {
+                dfs(grid, i, j)
+                islands++
+            }
+        }
+    }
+    return islands
+};
+var dfs = function (grid, i, j) {
+    if (
+            i < 0 ||
+            j < 0 ||
+            i >= grid.length ||
+            j >= grid[0].length ||
+            grid[i][j] === '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+}
